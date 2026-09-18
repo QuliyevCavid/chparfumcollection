df4bb1e034f81b5b1327064469abbfc21e3c12dc
QuliyevCavid <quliyevv992@gmail.com>
2026-09-18 18:55:38 +0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://jentxbrreapsydfyxdzw.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_nc4ZfflYnW4fSzZjOVEUDg_rC9_G6Dv";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
